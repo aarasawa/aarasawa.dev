b386f897b8a463787a014b60642339bae5606bb4
Alex Arasawa <aarasawa@gmail.com>
2024-01-09 01:29:09 -0800

    added fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { IBM_Plex_Sans, VT323, Press_Start_2P } from 'next/font/google';
+
+export const ibm = IBM_Plex_Sans({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export const vt323 = VT323({
+    weight: ['400'],
+    subsets: ['latin'],
+});
+
+export const  start_2p = Press_Start_2P({
+    weight: ['400'],
+    subsets: ['latin'],
+});
