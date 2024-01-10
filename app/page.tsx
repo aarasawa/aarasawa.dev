9dda8b578322f0be076e841df11a809fb8b38113
Alex Arasawa <aarasawa@gmail.com>
2024-01-09 19:00:40 -0800

    adding title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+    <html>
+      <header>
+        <h1>Alex Arasawa</h1>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        
+      </main>
+    </html>
   )
 }
