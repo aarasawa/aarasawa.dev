9e16af8f605e65a1d0566fb0b95be0839f4952d3
Alex Arasawa <aarasawa@gmail.com>
2024-01-13 21:54:53 -0800

    testing out other colors, transferring styles over to main pc

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ const socials = [
     icon: <Linkedin size={20} />,
     href:"https://linkedin.com/in/alexander-arasawa",
     label: "LinkedIn",
-    handle: "Alexander Arasawa",
+    handle: "Alex Arasawa",
   },
 	{
 		icon: <Mail size={20} />,
@@ -27,7 +27,7 @@ const socials = [
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className="bg-gradient-to-tr from-black via-emerald-600/10 to-black scroll-p-5">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
@@ -38,10 +38,6 @@ export default function Example() {
 								target="_blank"
 								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
-								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-									aria-hidden="true"
-								/>
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
 								</span>{" "}
