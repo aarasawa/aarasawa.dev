89df7496c59a9c692d14d2100963ba559ccad45c
Alex Arasawa <aarasawa@gmail.com>
2024-02-08 17:45:38 -0800

    commiting previous edits to style and potential boot sequence

---
+++ app/boot/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+function BootUp() {
+  const router = useRouter();
+
+
+}
