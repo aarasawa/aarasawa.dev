8d5cb3dcb4f2bce2f3c1622a6fec3deba7b7b8c4
Alex Arasawa <aarasawa@gmail.com>
2024-02-16 20:48:23 -0800

    attempting to implement single project card, pushing to work on later

--- app/components/projects_card.tsx
+++ app/components/projects_card.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren } from "react";
 export const Projects_Card: React.FC<PropsWithChildren> = ({children}) => {
   return (
     <div className="pointer-events-auto justify-self-center overflow-hidden relative group w-[334px] h-[173px]">
+      {children}
       <div className="w-[331px] h-[170px] left-[3px] top-[3px] absolute bg-neutral-500 group-hover:bg-neutral-400"></div>
       <div className="w-[329px] h-[168px] left-0 top-0 absolute">
         <div className="w-[329px] h-[168px] left-0 top-0 absolute bg-black border border-zinc-400 group-hover:border-white"></div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,18 +17,18 @@ export default function Projects() {
 		<div className="bg-black scroll-p-5">
 			<Navigation/>
 			<div className="container flex items-center min-h-screen py-16 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 p-8">
+				<div className="grid w-full grid-cols-1 gap-8">
 					{projects.map((p) => (
 						<Projects_Card key={p.label}>
 							<Link
 								href={p.href}
 								className="overflow-hidden relative flex flex-col items-center gap-4 py-24 group"
 							>
-								<span className="relative z-10 flex items-center">
+								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm border group-hover:text-white group-hover:border-white text-zinc-400 border-zinc-500">
 									{p.icon}
 								</span>
 								<div className="z-10 flex flex-col items-center">
-								<span className="font-medium xl:text-xl text-zinc-400 group-hover:text-white">
+									<span className="font-medium xl:text-xl text-zinc-400 group-hover:text-white">
 										{p.handle}
 									</span>
 									<span className="z-50 text-sm text-center text-zinc-400 group-hover:text-zinc-200">
