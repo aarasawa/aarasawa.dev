3baf2f9327fa03bc4e091dab69f9f0eabd206fc9
Alex Arasawa <aarasawa@gmail.com>
2024-03-29 19:12:44 -0700

    Working on an additional animation for the title on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className=" flex flex-col justify-center w-screen h-screen overflow-hidden">
+    <div className="flex flex-col justify-center w-screen h-screen overflow-hidden">
       <div className="bg-black">
         <div className="flex"></div>
-        <h1 className="text-center mt-5 text-4xl text-transparent duration-1000 bg-white cursor-default animate-title font-display sm:text-6xl md:text-6xl whitespace-nowrap bg-clip-text">
+        <h1 className="text-center mt-5 text-transparent bg-white cursor-default font-display sm:text-6xl md:text-6xl whitespace-nowrap bg-clip-text">
             Alexander Arasawa
         </h1>
         <nav className="my-3 animate-fade-in">
