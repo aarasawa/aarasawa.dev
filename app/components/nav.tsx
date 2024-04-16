fa4e1922a2de15e5c6c58d3426ed600b82eb9fcf
Alex Arasawa <aarasawa@gmail.com>
2024-04-15 23:20:25 -0700

    UPDATE: created a hamburger menu for mobile screens

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { Home } from "lucide-react";
+import { Home, Menu, X } from "lucide-react";
 import Link from 'next/link';
 import React, { useEffect, useRef, useState } from 'react';
 
 const navigation = [
-	{ name: "Preloader Testing Module", href: "/boot"},
+	{ name: "Preloader", href: "/boot"},
 	{ name: "Projects", href: "/projects"},
 	{ name: "Contact", href: "/contact"},
 ];
@@ -12,6 +12,8 @@ const navigation = [
 export const Navigation: React.FC = () => {
   const ref = useRef<HTMLElement>(null);
 	const [isIntersecting, setIntersecting] = useState(true);
+	const [isMobile, setIsMobile] = useState(false);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	useEffect(() => {
 		if (!ref.current) return;
@@ -23,32 +25,80 @@ export const Navigation: React.FC = () => {
 		return () => observer.disconnect();
 	}, []);
 
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 768);
+		};
+
+		handleResize();
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
 	return (
 		<header ref={ref}>
+			{isMenuOpen && <div className="fixed inset-0 backdrop-blur-xl z-50"></div>}
 			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
+				className={`fixed inset-x-0 top-0 z-50 border-b ${
 					isIntersecting
 						? "bg-zinc-900/0 border-transparent"
-						: "bg-zinc-900/500  border-zinc-800 "
+						: "bg-zinc-900/500  border-zinc-800"
 				}`}
 			>
-				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto text-xl">
-					<div className="flex justify-between gap-8">
-						{navigation.map((item) => (
-							<Link
-								key={item.href}
-								href={item.href}
-								className="text-zinc-400 hover:text-zinc-100"
+				<div className="container flex items-center justify-between p-6 mx-auto text-xl relative">
+					{isMobile ? (
+						<>
+							<button 
+								className="text-zinc-300 hover:text-zinc-100"
+								onClick={toggleMenu}
 							>
-								{item.name}
-							</Link>
-						))}
-					</div>
+								{isMenuOpen ? <X/> : <Menu/>}
+							</button>
+							{isMenuOpen && (
+								<div className="fixed inset-0 flex justify-center items-center z-50">
+									<div className="flex flex-col items-center gap-4 bg-zinc-900/500 p-4">
+										{navigation.map((item, index) => (
+											<React.Fragment key={item.href}>
+												<Link
+													href={item.href}
+													className="text-zinc-400 hover:text-zinc-100"
+												>
+													{item.name}
+												</Link>
+												{index !== navigation.length - 1 && (
+													<div className="h-px w-full bg-zinc-600"/>
+												)}
+											</React.Fragment>
+										))}
+									</div>
+								</div>
+							)}
+						</>
+					) : (
+						<div className="flex justify-between gap-8">
+							{navigation.map((item) => (
+								<Link
+									key={item.href}
+									href={item.href}
+									className="text-zinc-400 hover:text-zinc-100"
+								>
+									{item.name}
+								</Link>
+							))}
+						</div>
+					)}
 					<Link
 						href="/"
-						className="duration-200 text-zinc-300 hover:text-zinc-100"
+						className="text-zinc-300 hover:text-zinc-100"
 					>
-						<Home className="w-6 h-6 "/>
+						<Home/>
 					</Link>
 				</div>
 			</div>
